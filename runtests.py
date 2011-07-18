92b28dd2e6699423707604146de4a29656b8a8ba
Charles Leifer <coleifer@gmail.com>
2011-07-17 20:42:14 -0500

    Return the proper exit codes in event of failure or error

--- runtests.py
+++ runtests.py
@@ -9,7 +9,11 @@ import tests
 def runtests(*test_args):
     suite = unittest.TestLoader().loadTestsFromModule(tests)
     result = unittest.TextTestRunner(verbosity=2).run(suite)
-    sys.exit(bool(result.failures))
+    if result.failures:
+        sys.exit(1)
+    elif result.errors:
+        sys.exit(2)
+    sys.exit(0)
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
