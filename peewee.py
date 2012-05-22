e6b199074023237d9c1bdeef5bb2b13d3b47fd13
Charles Leifer <coleifer@gmail.com>
2012-05-22 15:46:02 -0500

    Adding a simplified function for finding subclasses recursively, thanks @tmoertel for the suggestion

--- peewee.py
+++ peewee.py
@@ -2734,3 +2734,11 @@ class Model(object):
 
         for field_name in fields:
             setattr(self, field_name, getattr(obj, field_name))
+
+def find_subclasses(klass, include_self=False):
+    accum = []
+    for child in klass.__subclasses__():
+        accum.extend(find_subclasses(child, True))
+    if include_self:
+        accum.append(klass)
+    return accum
