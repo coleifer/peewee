eaa273df1d5b92733985e34506c2af716b3e0610
Charles Leifer <coleifer@gmail.com>
2013-04-18 08:31:05 -0500

    support rich get/set in sqlite kv

--- playhouse/sqlite_kv.py
+++ playhouse/sqlite_kv.py
@@ -4,6 +4,7 @@ Provides a simple, in-memory key/value store using Sqlite.
 
 import pickle
 from peewee import *
+from peewee import Leaf
 
 try:
     from playhouse.apsw_ext import APSWDatabase
@@ -39,24 +40,40 @@ class KeyStore(object):
     def __len__(self):
         return self.model.select().count()
 
-    def __getitem__(self, key):
-        kv = self.model
-        try:
-            res = self.query(kv.value).where(kv.key == key).get()
-        except kv.DoesNotExist:
-            raise KeyError(key)
-        return pickle.loads(res[0])
+    def convert_expr(self, expr):
+        if not isinstance(expr, Leaf):
+            return (self.model.key == expr), True
+        return expr, False
 
-    def __setitem__(self, key, value):
+    def __getitem__(self, expr):
+        kv = self.model
+        converted, is_single = self.convert_expr(expr)
+        query = self.query(kv.value).where(converted)
+        result = [pickle.loads(item[0]) for item in query]
+        if len(result) == 0 and is_single:
+            raise KeyError(expr)
+        elif is_single:
+            return result[0]
+        return result
+
+    def upsert(self, key, value):
         sets, params = self._compiler.parse_field_dict({
             self.model.key: key,
-            self.model.value: pickle.dumps(value)})
+            self.model.value: value})
         fields, interp = zip(*sets)
         sql = 'INSERT OR REPLACE INTO %s (%s) VALUES (%s)' % (
             self._compiler.quote(self.model._meta.db_table),
             ', '.join(fields),
             ', '.join(interp))
         self._db.execute_sql(sql, params, True)
+
+    def __setitem__(self, expr, value):
+        pickled_value = pickle.dumps(value)
+        if isinstance(expr, Leaf):
+            kv = self.model
+            kv.update(**{kv.value: pickled_value}).where(expr).execute()
+        else:
+            self.upsert(expr, pickled_value)
 
     def __delitem__(self, key):
         self.model.delete().where(self.model.key == key).execute()
