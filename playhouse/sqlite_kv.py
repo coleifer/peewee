339400759ecf8621db54e7ea291bf037cd6cd741
Charles Leifer <coleifer@gmail.com>
2013-05-19 21:10:45 -0500

    Enhancements to the sqlite k/v

--- playhouse/sqlite_kv.py
+++ playhouse/sqlite_kv.py
@@ -3,6 +3,7 @@ Provides a simple, in-memory key/value store using Sqlite.
 """
 
 import itertools
+import operator
 import pickle
 from peewee import *
 from peewee import Leaf
@@ -19,42 +20,68 @@ Sentinel = type('Sentinel', (object,), {})
 
 key_value_db = KeyValueDatabase(':memory:')
 
+class PickleField(BlobField):
+    def db_value(self, value):
+        return pickle.dumps(value)
 
-class KV(Model):
-    key = CharField(index=True, primary_key=True)
-    value = BlobField()
-
-    class Meta:
-        database = key_value_db
-
+    def python_value(self, value):
+        return pickle.loads(value)
 
 class KeyStore(object):
-    def __init__(self, ordered=False, model=None):
-        self.model = model or KV
+    """
+    Rich dictionary with support for storing a wide variety of data types.
+
+    :param peewee.Field value_type: Field type to use for values.
+    :param boolean ordered: Whether keys should be returned in sorted order.
+    :param peewee.Model model: Model class to use for Keys/Values.
+    """
+    def __init__(self, value_field, ordered=False, database=None):
+        self._value_field = value_field
+        self._ordered = ordered
+
+        self._database = database or key_value_db
+        self._compiler = self._database.compiler()
+
+        self.model = self.create_model()
         self.key = self.model.key
         self.value = self.model.value
 
-        self._db = self.model._meta.database
-        self._compiler = self._db.compiler()
-        self._ordered = ordered
+        self._database.create_table(self.model, True)
+        self._native_upsert = isinstance(self._database, SqliteDatabase)
 
-        self._db.create_table(self.model, True)
+    def create_model(self):
+        class KVModel(Model):
+            key = CharField(max_length=255, primary_key=True)
+            value = self._value_field
 
-    def __contains__(self, key):
-        return self.model.select().where(self.key == key).exists()
+            class Meta:
+                database = self._database
 
-    def __len__(self):
-        return self.model.select().count()
+        return KVModel
+
+    def query(self, *select):
+        query = self.model.select(*select).tuples()
+        if self._ordered:
+            query = query.order_by(self.key)
+        return query
 
     def convert_expr(self, expr):
         if not isinstance(expr, Leaf):
             return (self.key == expr), True
         return expr, False
 
+    def __contains__(self, key):
+        expr, _ = self.convert_expr(key)
+        return self.model.select().where(expr).exists()
+
+    def __len__(self):
+        return self.model.select().count()
+
     def __getitem__(self, expr):
         converted, is_single = self.convert_expr(expr)
-        query = self.query(self.value).where(converted)
-        result = [pickle.loads(item[0]) for item in query]
+        result = self.query(self.value).where(converted)
+        item_getter = operator.itemgetter(0)
+        result = [item_getter(val) for val in result]
         if len(result) == 0 and is_single:
             raise KeyError(expr)
         elif is_single:
@@ -70,37 +97,30 @@ class KeyStore(object):
             self._compiler.quote(self.model._meta.db_table),
             ', '.join(fields),
             ', '.join(interp))
-        self._db.execute_sql(sql, params, True)
+        self._database.execute_sql(sql, params, True)
 
     def __setitem__(self, expr, value):
-        pickled_value = pickle.dumps(value)
         if isinstance(expr, Leaf):
-            update = {self.value.name: pickled_value}
+            update = {self.value.name: value}
             self.model.update(**update).where(expr).execute()
+        elif self._native_upsert:
+            self._upsert(expr, value)
         else:
-            self._upsert(expr, pickled_value)
+            with self._database.transaction():
+                pass
 
     def __delitem__(self, expr):
         converted, _ = self.convert_expr(expr)
         self.model.delete().where(converted).execute()
 
-    def query(self, *select):
-        query = self.model.select(*select).tuples()
-        if self._ordered:
-            query = query.order_by(self.key)
-        return query
-
     def __iter__(self):
-        for k, v in self.query().execute():
-            yield k, pickle.loads(v)
+        return self.query().execute()
 
     def keys(self):
-        for row in self.query(self.key):
-            yield row[0]
+        return map(operator.itemgetter(0), self.query(self.key))
 
     def values(self):
-        for row in self.query(self.value):
-            yield pickle.loads(row[0])
+        return map(operator.itemgetter(0), self.query(self.value))
 
     def items(self):
         return iter(self)
@@ -112,7 +132,7 @@ class KeyStore(object):
             return default
 
     def pop(self, k, default=Sentinel):
-        with self._db.transaction():
+        with self._database.transaction():
             expr, is_single = self.convert_expr(k)
             try:
                 res = self[k]
@@ -125,3 +145,8 @@ class KeyStore(object):
 
     def clear(self):
         self.model.delete().execute()
+
+
+class PickledKeyStore(KeyStore):
+    def __init__(self, ordered=False, database=None):
+        super(PickledKeyStore, self).__init__(PickleField(), ordered, database)
--- playhouse/tests_sqlite_kv.py
+++ playhouse/tests_sqlite_kv.py
@@ -2,20 +2,22 @@ import threading
 import unittest
 
 from peewee import *
+from playhouse.sqlite_kv import PickledKeyStore
 from playhouse.sqlite_kv import KeyStore
 
 
 class KeyStoreTestCase(unittest.TestCase):
     def setUp(self):
-        self.kv = KeyStore()
-        self.ordered_kv = KeyStore(ordered=True)
+        self.kv = KeyStore(CharField())
+        self.ordered_kv = KeyStore(CharField(), ordered=True)
+        self.pickled_kv = PickledKeyStore(ordered=True)
         self.kv.clear()
 
     def test_storage(self):
         self.kv['a'] = 'A'
         self.kv['b'] = 1
         self.assertEqual(self.kv['a'], 'A')
-        self.assertEqual(self.kv['b'], 1)
+        self.assertEqual(self.kv['b'], '1')
         self.assertRaises(KeyError, self.kv.__getitem__, 'c')
 
         del(self.kv['a'])
@@ -27,13 +29,18 @@ class KeyStoreTestCase(unittest.TestCase):
 
         self.kv[self.kv.key << ('a', 'c')] = 'X'
         self.assertEqual(self.kv['a'], 'X')
-        self.assertEqual(self.kv['b'], 1)
+        self.assertEqual(self.kv['b'], '1')
         self.assertEqual(self.kv['c'], 'X')
 
         del(self.kv[self.kv.key << ('a', 'c')])
         self.assertRaises(KeyError, self.kv.__getitem__, 'a')
         self.assertRaises(KeyError, self.kv.__getitem__, 'c')
-        self.assertEqual(self.kv['b'], 1)
+        self.assertEqual(self.kv['b'], '1')
+
+        self.pickled_kv['a'] = 'A'
+        self.pickled_kv['b'] = 1.1
+        self.assertEqual(self.pickled_kv['a'], 'A')
+        self.assertEqual(self.pickled_kv['b'], 1.1)
 
     def test_container_properties(self):
         self.kv['x'] = 'X'
@@ -43,35 +50,37 @@ class KeyStoreTestCase(unittest.TestCase):
         self.assertFalse('a' in self.kv)
 
     def test_dict_methods(self):
-        self.ordered_kv['a'] = 'A'
-        self.ordered_kv['c'] = 'C'
-        self.ordered_kv['b'] = 'B'
-        self.assertEqual(list(self.ordered_kv.keys()), ['a', 'b', 'c'])
-        self.assertEqual(list(self.ordered_kv.values()), ['A', 'B', 'C'])
-        self.assertEqual(list(self.ordered_kv.items()), [
-            ('a', 'A'),
-            ('b', 'B'),
-            ('c', 'C'),
-        ])
+        for kv in (self.ordered_kv, self.pickled_kv):
+            kv['a'] = 'A'
+            kv['c'] = 'C'
+            kv['b'] = 'B'
+            self.assertEqual(list(kv.keys()), ['a', 'b', 'c'])
+            self.assertEqual(list(kv.values()), ['A', 'B', 'C'])
+            self.assertEqual(list(kv.items()), [
+                ('a', 'A'),
+                ('b', 'B'),
+                ('c', 'C'),
+            ])
 
     def test_iteration(self):
-        self.ordered_kv['a'] = 'A'
-        self.ordered_kv['c'] = 'C'
-        self.ordered_kv['b'] = 'B'
+        for kv in (self.ordered_kv, self.pickled_kv):
+            kv['a'] = 'A'
+            kv['c'] = 'C'
+            kv['b'] = 'B'
 
-        items = list(self.ordered_kv)
-        self.assertEqual(items, [
-            ('a', 'A'),
-            ('b', 'B'),
-            ('c', 'C'),
-        ])
+            items = list(kv)
+            self.assertEqual(items, [
+                ('a', 'A'),
+                ('b', 'B'),
+                ('c', 'C'),
+            ])
 
     def test_shared_mem(self):
         self.kv['a'] = 'xxx'
         self.assertEqual(self.ordered_kv['a'], 'xxx')
 
         def set_k():
-            kv_t = KeyStore()
+            kv_t = KeyStore(CharField())
             kv_t['b'] = 'yyy'
         t = threading.Thread(target=set_k)
         t.start()
