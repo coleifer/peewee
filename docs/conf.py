0ec5e726ed028271c201fbfd2c4ae9a0bc353e9a
Charles Leifer <coleifer@gmail.com>
2013-11-04 13:45:30 -0600

    Use new theme.

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+RTD_NEW_THEME = True
+
 import sys, os
 
 # If extensions (or modules to document with autodoc) are in another directory,
