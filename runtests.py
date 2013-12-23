e4684ff01631047da78a0f5ea2d3dff94416047f
Charles Leifer <coleifer@gmail.com>
2013-12-23 16:50:02 -0600

    Moving proxy tests.

--- runtests.py
+++ runtests.py
@@ -31,7 +31,6 @@ def get_option_parser():
     cases.add_option('--kv', dest='kv', default=False, action='store_true', help='key/value store tests')
     cases.add_option('--migrations', dest='migrations', default=False, action='store_true', help='migration helper tests (requires psycopg2)')
     cases.add_option('--postgres-ext', dest='postgres_ext', default=False, action='store_true', help='postgres_ext tests (requires psycopg2)')
-    cases.add_option('--proxy', dest='proxy', default=False, action='store_true', help='proxy tests')
     cases.add_option('--pwiz', dest='pwiz', default=False, action='store_true', help='pwiz, schema introspector and model generator')
     cases.add_option('--signals', dest='signals', default=False, action='store_true', help='signals tests')
     cases.add_option('--shortcuts', dest='shortcuts', default=False, action='store_true', help='shortcuts tests')
@@ -73,9 +72,6 @@ def collect_modules(options):
             modules.append(tests_postgres)
         except ImportError:
             print_('Unable to import postgres-ext tests, skipping')
-    if xtra(options.proxy):
-        from playhouse import tests_proxy
-        modules.append(tests_proxy)
     if xtra(options.pwiz):
         from playhouse import tests_pwiz
         modules.append(tests_pwiz)
--- tests.py
+++ tests.py
@@ -1022,6 +1022,41 @@ class ValidationTestCase(BasePeeweeTestCase):
         for val in vals_to_try:
             self.assertRaises(TypeError, declare_bad, val)
 
+
+class ProxyTestCase(BasePeeweeTestCase):
+    def test_proxy(self):
+        class A(object):
+            def foo(self):
+                return 'foo'
+
+        a = Proxy()
+        def raise_error():
+            a.foo()
+        self.assertRaises(AttributeError, raise_error)
+
+        a.initialize(A())
+        self.assertEqual(a.foo(), 'foo')
+
+    def test_proxy_database(self):
+        database_proxy = Proxy()
+
+        class DummyModel(TestModel):
+            test_field = CharField()
+            class Meta:
+                database = database_proxy
+
+        # Un-initialized will raise an AttributeError.
+        self.assertRaises(AttributeError, DummyModel.create_table)
+
+        # Initialize the object.
+        database_proxy.initialize(SqliteDatabase(':memory:'))
+
+        # Do some queries, verify it is working.
+        DummyModel.create_table()
+        DummyModel.create(test_field='foo')
+        self.assertEqual(DummyModel.get().test_field, 'foo')
+        DummyModel.drop_table()
+
 #
 # TEST CASE USED TO PROVIDE ACCESS TO DATABASE
 # FOR EXECUTION OF "LIVE" QUERIES
