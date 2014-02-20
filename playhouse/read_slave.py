893449d13ba15697f6f61ee12307ee1e5a350ae3
Charles Leifer <coleifer@gmail.com>
2014-02-19 23:05:47 -0600

    Multiple read slaves

--- playhouse/read_slave.py
+++ playhouse/read_slave.py
@@ -9,7 +9,7 @@ read_slave = PostgresqlDatabase('read_slave')
 class BaseModel(ReadSlaveModel):
     class Meta:
         database = master
-        read_slave = read_slave  # This database will be used for SELECTs.
+        read_slaves = [read_slave]  # This database will be used for SELECTs.
 
 
 # Now define your models as you would normally.
@@ -25,16 +25,22 @@ from peewee import *
 
 class ReadSlaveModel(Model):
     @classmethod
+    def _get_read_database(cls):
+        if not getattr(cls._meta, 'read_slaves', None):
+            return cls._meta.database
+        current_idx = getattr(cls, '_read_slave_idx', -1)
+        cls._read_slave_idx = (current_idx + 1) % len(cls._meta.read_slaves)
+        return cls._meta.read_slaves[cls._read_slave_idx]
+
+    @classmethod
     def select(cls, *args, **kwargs):
         query = super(ReadSlaveModel, cls).select(*args, **kwargs)
-        if getattr(cls._meta, 'read_slave', None):
-            query.database = cls._meta.read_slave
+        query.database = cls._get_read_database()
         return query
 
     @classmethod
     def raw(cls, *args, **kwargs):
         query = super(ReadSlaveModel, cls).raw(*args, **kwargs)
-        if (getattr(cls._meta, 'read_slave', None) and
-                query._sql.lower().startswith('select')):
-            query.database = cls._meta.read_slave
+        if query._sql.lower().startswith('select'):
+            query.database = cls._get_read_database()
         return query
--- playhouse/tests_read_slave.py
+++ playhouse/tests_read_slave.py
@@ -21,50 +21,81 @@ class QueryLogDatabase(SqliteDatabase):
 class Master(QueryLogDatabase):
     name = 'master'
 
-class Slave(QueryLogDatabase):
-    name = 'slave'
+class Slave1(QueryLogDatabase):
+    name = 'slave1'
+
+class Slave2(QueryLogDatabase):
+    name = 'slave2'
 
 master = Master('tmp.db')
-slave = Slave('tmp.db')
+slave1 = Slave1('tmp.db')
+slave2 = Slave2('tmp.db')
 
 class BaseModel(ReadSlaveModel):
     class Meta:
         database = master
-        read_slave = slave
+        read_slaves = [slave1, slave2]
 
 class User(BaseModel):
     username = CharField()
+
+class Thing(BaseModel):
+    name = CharField()
+
+    class Meta:
+        read_slaves = [slave2]
 
 class TestMasterSlave(unittest.TestCase):
     def setUp(self):
         User.drop_table(True)
         User.create_table()
+        Thing.drop_table(True)
+        Thing.create_table()
+        User.create(username='peewee')
+        Thing.create(name='something')
         reset()
 
     def tearDown(self):
         User.drop_table(True)
+        Thing.drop_table(True)
 
     def assertQueries(self, databases):
         self.assertEqual([q[0] for q in queries], databases)
 
-    def test_database(self):
+    def test_balance_pair(self):
+        for i in range(6):
+            User.get()
+        self.assertQueries([
+            'slave1',
+            'slave2',
+            'slave1',
+            'slave2',
+            'slave1',
+            'slave2'])
+
+    def test_balance_single(self):
+        for i in range(3):
+            Thing.get()
+        self.assertQueries(['slave2', 'slave2', 'slave2'])
+
+    def test_query_types(self):
         u = User.create(username='charlie')
         User.select().where(User.username == 'charlie').get()
-        self.assertQueries(['master', 'slave'])
+        self.assertQueries(['master', 'slave1'])
 
         User.get(User.username == 'charlie')
-        self.assertQueries(['master', 'slave', 'slave'])
+        self.assertQueries(['master', 'slave1', 'slave2'])
 
         u.username = 'edited'
         u.save()  # Update.
-        self.assertQueries(['master', 'slave', 'slave', 'master'])
+        self.assertQueries(['master', 'slave1', 'slave2', 'master'])
 
         u.delete_instance()
-        self.assertQueries(['master', 'slave', 'slave', 'master', 'master'])
+        self.assertQueries(['master', 'slave1', 'slave2', 'master', 'master'])
 
     def test_raw_queries(self):
         User.raw('insert into user (username) values (?)', 'charlie').execute()
-        rq = list(User.raw('select * from user'))
+        rq = list(User.raw('select * from user where username = ?', 'charlie'))
         self.assertEqual(rq[0].username, 'charlie')
 
-        self.assertQueries(['master', 'slave'])
+        self.assertQueries(['master', 'slave1'])
