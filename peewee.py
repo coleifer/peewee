c09b074362c49ab5e9642734e8f28c71fb1486af
Charles Leifer <coleifer@gmail.com>
2014-07-30 23:41:27 -0500

    Adding quotes to peewee.

--- peewee.py
+++ peewee.py
@@ -1,3 +1,11 @@
+# May you do good and not evil
+# May you find forgiveness for yourself and forgive others
+# May you share freely, never taking more than you give.  -- SQLite source code
+#
+# As we enjoy great advantages from the inventions of others, we should be glad
+# of an opportunity to serve others by an invention of ours, and this we should
+# do freely and generously.  -- Ben Franklin
+#
 #     (\
 #     (  \  /(o)\     caw!
 #     (   \/  ()/ /)
@@ -7,6 +15,7 @@
 #      ///'
 #     //
 #    '
+
 import datetime
 import decimal
 import hashlib
