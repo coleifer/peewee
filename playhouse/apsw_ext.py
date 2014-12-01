4f058899fc96baf963349d97079a479c6dd68754
Charles Leifer <coleifer@gmail.com>
2014-12-01 01:52:09 -0600

    As of 3.8.4.3-r1, APSW offers fetchone(), and 3.7.12-r1 added description. No more hacky wrappers.

--- playhouse/apsw_ext.py
+++ playhouse/apsw_ext.py
@@ -30,43 +30,6 @@ from peewee import TimeField as _TimeField
 from peewee import transaction as _transaction
 
 
-class ConnectionWrapper(apsw.Connection):
-    def cursor(self):
-        base_cursor = super(ConnectionWrapper, self).cursor()
-        return CursorProxy(base_cursor)
-
-
-class CursorProxy(object):
-    def __init__(self, cursor_obj):
-        self.cursor_obj = cursor_obj
-        self.implements = set(['description', 'fetchone'])
-
-    def __getattr__(self, attr):
-        if attr in self.implements:
-            return self.__getattribute__(attr)
-        return getattr(self.cursor_obj, attr)
-
-    @property
-    def description(self):
-        try:
-            return self.cursor_obj.getdescription()
-        except apsw.ExecutionCompleteError:
-            return []
-
-    if PY3:
-        def fetchone(self):
-            try:
-                return next(self.cursor_obj)
-            except StopIteration:
-                pass
-    else:
-        def fetchone(self):
-            try:
-                return self.cursor_obj.next()
-            except StopIteration:
-                pass
-
-
 class transaction(_transaction):
     def __init__(self, db, lock_type='deferred'):
         self.db = db
@@ -89,7 +52,7 @@ class APSWDatabase(SqliteDatabase):
         del(self._modules[mod_name])
 
     def _connect(self, database, **kwargs):
-        conn = ConnectionWrapper(database, **kwargs)
+        conn = apsw.Connection(database, **kwargs)
         if self.timeout is not None:
             conn.setbusytimeout(self.timeout)
         conn.createscalarfunction('date_part', _sqlite_date_part, 2)
