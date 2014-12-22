f7edb18764abf16260b92a479ee018cb6584d937
Charles Leifer <coleifer@gmail.com>
2014-12-21 23:52:06 -0600

    Version 2.4.5

--- peewee.py
+++ peewee.py
@@ -31,7 +31,7 @@ from copy import deepcopy
 from functools import wraps
 from inspect import isclass
 
-__version__ = '2.4.4'
+__version__ = '2.4.5'
 __all__ = [
     'BareField',
     'BigIntegerField',
