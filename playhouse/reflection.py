425a72ff999a9cea3111d8b64223be145712bc65
Dmytro Korduban <dkorduban.ua@gmail.com>
2014-12-28 17:09:26 +0200

    Fix Introspector schema handling.

--- playhouse/reflection.py
+++ playhouse/reflection.py
@@ -381,7 +381,7 @@ class Introspector(object):
 
     def introspect(self, table_names=None):
         # Retrieve all the tables in the database.
-        tables = self.metadata.database.get_tables()
+        tables = self.metadata.database.get_tables(self.schema)
         if table_names is not None:
             tables = [table for table in tables if table in table_names]
 
