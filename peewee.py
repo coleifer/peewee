6087199320a3ad8968add5d50f6c3a047767afdd
Charles Leifer <coleifer@gmail.com>
2016-01-13 10:25:45 -0600

    Commit_select for MySQL.

--- peewee.py
+++ peewee.py
@@ -3839,6 +3839,7 @@ class PostgresqlDatabase(Database):
         self.execute_sql('SET search_path TO %s' % path_params, search_path)
 
 class MySQLDatabase(Database):
+    commit_select = True
     compound_operations = ['UNION', 'UNION ALL']
     field_overrides = {
         'bool': 'BOOL',
