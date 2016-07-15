3cc3b70cad12aeaef536a6757130884efbb051c6
Charles Leifer <coleifer@gmail.com>
2016-07-14 22:34:31 -0500

    Include C sources as fallback for building extensions. Fixes #1007.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import warnings
+from distutils.command import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
 from distutils.version import StrictVersion
@@ -9,49 +10,26 @@ f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
 f.close()
 
-setup_kwargs = {}
-cython_min_version = '0.22.1'
+cython_min = '0.22.1'
 try:
     from Cython.Distutils import build_ext
-    from Cython import __version__ as cython_version
+    from Cython import __version__ as cython_ver
 except ImportError:
     cython_installed = False
-    warnings.warn('Cython C extensions for peewee will NOT be built, because '
-                  'Cython does not seem to be installed. To enable Cython C '
-                  'extensions, install Cython >=' + cython_min_version + '.')
 else:
-    if StrictVersion(cython_version) < StrictVersion(cython_min_version):
-        cython_installed = False
-        warnings.warn('Cython C extensions for peewee will NOT be built, '
-                      'because the installed Cython version '
-                      '(' + cython_version + ') is too old. To enable Cython '
-                      'C extensions, install Cython >=' + cython_min_version +
-                      '.')
-    else:
-        cython_installed = True
+    cython_installed = StrictVersion(cython_ver) >= StrictVersion(cython_min)
 
-speedups_ext_module = Extension(
-    'playhouse._speedups',
-    ['playhouse/_speedups.pyx'])
-sqlite_udf_module = Extension(
-    'playhouse._sqlite_udf',
-    ['playhouse/_sqlite_udf.pyx'])
-sqlite_ext_module = Extension(
-    'playhouse._sqlite_ext',
-    ['playhouse/_sqlite_ext.pyx'])
+extensions = (
+    ('playhouse._speedups', ('playhouse/_speedups.pyx',
+                             'playhouse/_speedups.c')),
+    ('playhouse._sqlite_udf', ('playhouse/_sqlite_udf.pyx',
+                               'playhouse/_sqlite_udf.c')),
+    ('playhouse._sqlite_ext', ('playhouse/_sqlite_ext.pyx',
+                               'playhouse/_sqlite_ext.c')),
+)
 
-
-ext_modules = []
-if cython_installed:
-    ext_modules.extend([
-        speedups_ext_module,
-        sqlite_udf_module,
-        sqlite_ext_module])
-
-if ext_modules:
-    setup_kwargs.update(
-        cmdclass={'build_ext': build_ext},
-        ext_modules=ext_modules)
+ext_modules = [Extension(module, [pyx if cython_installed else c])
+               for module, (pyx, c) in extensions]
 
 setup(
     name='peewee',
@@ -63,6 +41,8 @@ setup(
     url='http://github.com/coleifer/peewee/',
     packages=['playhouse'],
     py_modules=['peewee', 'pwiz'],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -72,5 +52,4 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     scripts = ['pwiz.py'],
-    **setup_kwargs
 )
