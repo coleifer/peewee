651d1c173c281be595e6a9053003973aa226b08e
Charles Leifer <charles.leifer@propylon.com>
2017-03-08 14:50:08 -0600

    Test context settings.

--- tests/database.py
+++ tests/database.py
@@ -1,3 +1,4 @@
+from peewee import attrdict
 from peewee import *
 
 from .base import DatabaseTestCase
@@ -14,3 +15,22 @@ class TestDatabase(DatabaseTestCase):
         self.assertEqual(db.foreign_keys, 1)
         db.foreign_keys = 'off'
         self.assertEqual(db.foreign_keys, 0)
+
+    def test_context_settings(self):
+        class TestDatabase(Database):
+            options = Database.options + attrdict(
+                field_types={
+                    FIELD.BIGINT: 'TEST_BIGINT',
+                    FIELD.TEXT: 'TEST_TEXT'},
+                param='$')
+
+        test_db = TestDatabase(None)
+        state = test_db.get_sql_context().state
+
+        self.assertEqual(state.field_types[FIELD.BIGINT], 'TEST_BIGINT')
+        self.assertEqual(state.field_types[FIELD.TEXT], 'TEST_TEXT')
+        self.assertEqual(state.field_types['INT'], FIELD.INT)
+        self.assertEqual(state.field_types['VARCHAR'], FIELD.VARCHAR)
+
+        self.assertEqual(state.param, '$')
+        self.assertEqual(state.quote, '"')
