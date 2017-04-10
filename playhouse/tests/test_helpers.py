fe8580c6c972d5e28ce393fe2c27cf14b3639a7e
Max Alibaev <o@ololo.im>
2017-04-10 03:54:57 +0300

    #1076 Simplified the test, since class D didn't change anything.

--- playhouse/tests/test_helpers.py
+++ playhouse/tests/test_helpers.py
@@ -112,19 +112,17 @@ class TestDeclaredDependencies(PeeweeTestCase):
             self.assertEqual(ordering, ordered)
     
     def test_declared_dependencies_2(self):
-        class D(Model):
-            pass
         class C(Model):
-            d = ForeignKeyField(D)
+            pass
         class B(Model):
             c = ForeignKeyField(C)
         class A(Model):
             class Meta:
                 depends_on = B,
             c = ForeignKeyField(C)
-        
-        models = [ D, C, B, A ]
-        
+
+        models = [ C, B, A ]
+
         ordered = list(models)
         for pmodels in permutations(models):
             ordering = sort_models_topologically(pmodels)
