d51990ab63d9de9c8a5307dc5278b40e07561850
Charles Leifer <coleifer@gmail.com>
2017-05-04 12:22:47 +0100

    Clean-up variable names.

--- tests/sqliteq.py
+++ tests/sqliteq.py
@@ -43,18 +43,18 @@ class BaseTestQueueDatabase(object):
         with db:
             User.create_table(True)
         User._meta.database = \
-                self.db = get_db(**self.database_config)
+                self.database = get_db(**self.database_config)
 
         # Sanity check at startup.
-        self.assertEqual(self.db.queue_size(), 0)
+        self.assertEqual(self.database.queue_size(), 0)
 
     def tearDown(self):
         super(BaseTestQueueDatabase, self).tearDown()
         User._meta.database = db
         with db:
             User.drop_table()
-        if not self.db.is_closed():
-            self.db.close()
+        if not self.database.is_closed():
+            self.database.close()
         if not db.is_closed():
             db.close()
         filename = db.database
@@ -62,16 +62,16 @@ class BaseTestQueueDatabase(object):
             os.unlink(filename)
 
     def test_query_error(self):
-        self.db.start()
-        curs = self.db.execute_sql('foo bar baz')
+        self.database.start()
+        curs = self.database.execute_sql('foo bar baz')
         self.assertRaises(OperationalError, curs.fetchone)
-        self.db.stop()
+        self.database.stop()
 
     def test_query_execution(self):
         qr = User.select().execute()
-        self.assertEqual(self.db.queue_size(), 0)
+        self.assertEqual(self.database.queue_size(), 0)
 
-        self.db.start()
+        self.database.start()
 
         users = list(qr)
         huey = User.create(name='huey')
@@ -79,9 +79,9 @@ class BaseTestQueueDatabase(object):
 
         self.assertTrue(huey.id is not None)
         self.assertTrue(mickey.id is not None)
-        self.assertEqual(self.db.queue_size(), 0)
+        self.assertEqual(self.database.queue_size(), 0)
 
-        self.db.stop()
+        self.database.stop()
 
     def create_thread(self, fn, *args):
         raise NotImplementedError
@@ -95,14 +95,14 @@ class BaseTestQueueDatabase(object):
                 User.create(name='u-%s' % i)
 
         total = self.n_threads * self.n_rows
-        self.db.start()
+        self.database.start()
         threads = [self.create_thread(create_rows, i, self.n_rows)
                    for i in range(0, total, self.n_rows)]
         [t.start() for t in threads]
         [t.join() for t in threads]
 
         self.assertEqual(User.select().count(), total)
-        self.db.stop()
+        self.database.stop()
 
     def test_pause(self):
         event_a = self.create_event()
@@ -115,7 +115,7 @@ class BaseTestQueueDatabase(object):
             else:
                 User.create(name=name)
 
-        self.db.start()
+        self.database.start()
 
         t_a = self.create_thread(create_user, 'a', event_a, True)
         t_a.start()
@@ -126,32 +126,32 @@ class BaseTestQueueDatabase(object):
         self.assertEqual(User.select().count(), 1)
 
         # Pause operations but preserve the writer thread/connection.
-        self.db.pause()
+        self.database.pause()
 
         event_a.set()
         self.assertEqual(User.select().count(), 1)
         t_a.join()
 
-        self.db.unpause()
+        self.database.unpause()
         self.assertEqual(User.select().count(), 1)
 
         event_b.set()
         t_b.join()
         self.assertEqual(User.select().count(), 2)
 
-        self.db.stop()
+        self.database.stop()
 
     def test_restart(self):
-        self.db.start()
+        self.database.start()
         User.create(name='a')
-        self.db.stop()
-        self.db._results_timeout = 0.0001
+        self.database.stop()
+        self.database._results_timeout = 0.0001
 
         self.assertRaises(ResultTimeout, User.create, name='b')
         self.assertEqual(User.select().count(), 1)
 
-        self.db.start()  # Will execute the pending "b" INSERT.
-        self.db._results_timeout = None
+        self.database.start()  # Will execute the pending "b" INSERT.
+        self.database._results_timeout = None
 
         User.create(name='c')
         self.assertEqual(User.select().count(), 3)
@@ -175,14 +175,14 @@ class BaseTestQueueDatabase(object):
         tg.start()
         threads.append(tg)
 
-        self.db.start()
+        self.database.start()
         [t.join() for t in threads]
-        self.db.stop()
+        self.database.stop()
 
         self.assertEqual(sorted(D), ['charlie', 'huey', 'users', 'zaizee'])
 
     def test_next_method(self):
-        self.db.start()
+        self.database.start()
 
         User.create(name='mickey')
         User.create(name='huey')
@@ -192,17 +192,17 @@ class BaseTestQueueDatabase(object):
         self.assertRaises(StopIteration, lambda: next(query))
 
         self.assertEqual(
-            next(self.db.execute_sql('PRAGMA journal_mode'))[0],
+            next(self.database.execute_sql('PRAGMA journal_mode'))[0],
             'wal')
 
-        self.db.stop()
+        self.database.stop()
 
 
 class TestThreadedDatabaseThreads(BaseTestQueueDatabase, BaseTestCase):
     database_config = {'use_gevent': False}
 
     def tearDown(self):
-        self.db._results_timeout = None
+        self.database._results_timeout = None
         super(TestThreadedDatabaseThreads, self).tearDown()
 
     def create_thread(self, fn, *args):
@@ -214,20 +214,21 @@ class TestThreadedDatabaseThreads(BaseTestQueueDatabase, BaseTestCase):
         return threading.Event()
 
     def test_timeout(self):
-        @self.db.func()
+        @self.database.func()
         def slow(n):
             time.sleep(n)
             return 'I slept for %s seconds' % n
 
-        self.db.start()
+        self.database.start()
 
         # Make the result timeout very small, then call our function which
         # will cause the query results to time-out.
-        self.db._results_timeout = 0.001
+        self.database._results_timeout = 0.001
         self.assertRaises(
             ResultTimeout,
-            lambda: self.db.execute_sql('select slow(?)', (0.005,)).fetchone())
-        self.db.stop()
+            lambda: self.database.execute_sql(
+                'select slow(?)', (0.005,), commit=True).fetchone())
+        self.database.stop()
 
 
 @skip_case_if(gevent is None)
