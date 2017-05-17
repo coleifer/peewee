b90bc0b9e564c51ae85f54a80207be67980fa097
Charles Leifer <coleifer@gmail.com>
2017-05-16 23:50:08 -0500

    Check for nonexistant pks in pwiz.

--- pwiz.py
+++ pwiz.py
@@ -111,6 +111,8 @@ def print_models(introspector, tables=None, preserve_order=False):
                 if col in primary_keys])
             pk_list = ', '.join("'%s'" % pk for pk in pk_field_names)
             print_('        primary_key = CompositeKey(%s)' % pk_list)
+        elif not primary_keys:
+            print_('        primary_key = False')
         print_('')
 
         seen.add(table)
