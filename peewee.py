45f73b7408417bca58e39c77399796f4d67b0eda
Charles Leifer <charles.leifer@propylon.com>
2017-05-30 16:49:58 -0500

    Attrdict fix backport.

--- peewee.py
+++ peewee.py
@@ -105,7 +105,11 @@ def __deprecated__(s):
 
 
 class attrdict(dict):
-    def __getattr__(self, attr): return self[attr]
+    def __getattr__(self, attr):
+        try:
+            return self[attr]
+        except KeyError:
+            raise AttributeError(attr)
     def __setattr__(self, attr, value): self[attr] = value
     def __iadd__(self, rhs): self.update(rhs); return self
     def __add__(self, rhs): d = attrdict(self); d.update(rhs); return d
