a3c5eed3365f22b218c945740b874cc11f7b97b5
Charles Leifer <charles.leifer@propylon.com>
2017-07-21 14:02:12 -0500

    Updating twitter and diary examples.

--- examples/twitter/run_example.py
+++ examples/twitter/run_example.py
@@ -3,5 +3,7 @@
 import sys
 sys.path.insert(0, '../..')
 
-from app import app
+from app import app, database, User, Relationship, Message
+with database:
+    database.create_tables([User, Relationship, Message])
 app.run()
