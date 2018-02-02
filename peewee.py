51cb6bdc25f62916fbc1f9bc471f1a1bccc1a7da
Charles Leifer <coleifer@gmail.com>
2018-02-02 08:31:53 -0600

    Support `coerce` in fn decorator. Replaces #1463.

--- peewee.py
+++ peewee.py
@@ -1225,8 +1225,8 @@ class Function(ColumnBase):
             self._coerce = coerce
 
     def __getattr__(self, attr):
-        def decorator(*args):
-            return Function(attr, args)
+        def decorator(*args, coerce=True):
+            return Function(attr, args, coerce=coerce)
         return decorator
 
     def over(self, partition_by=None, order_by=None, start=None, end=None,
