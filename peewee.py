1bc852ad097dc50af2201530781061f234a2f526
Charles Leifer <coleifer@gmail.com>
2018-03-30 10:22:29 -0500

    Capture column defaults in ColumnMetadata.

--- peewee.py
+++ peewee.py
@@ -2341,7 +2341,7 @@ IndexMetadata = collections.namedtuple(
     ('name', 'sql', 'columns', 'unique', 'table'))
 ColumnMetadata = collections.namedtuple(
     'ColumnMetadata',
-    ('name', 'data_type', 'null', 'primary_key', 'table'))
+    ('name', 'data_type', 'null', 'primary_key', 'table', 'default'))
 ForeignKeyMetadata = collections.namedtuple(
     'ForeignKeyMetadata',
     ('column', 'dest_table', 'dest_column', 'table'))
@@ -2905,8 +2905,8 @@ class SqliteDatabase(Database):
 
     def get_columns(self, table, schema=None):
         cursor = self.execute_sql('PRAGMA table_info("%s")' % table)
-        return [ColumnMetadata(row[1], row[2], not row[3], bool(row[5]), table)
-                for row in cursor.fetchall()]
+        return [ColumnMetadata(r[1], r[2], not r[3], bool(r[5]), table, r[4])
+                for r in cursor.fetchall()]
 
     def get_primary_keys(self, table, schema=None):
         cursor = self.execute_sql('PRAGMA table_info("%s")' % table)
@@ -3014,14 +3014,14 @@ class PostgresqlDatabase(Database):
 
     def get_columns(self, table, schema=None):
         query = """
-            SELECT column_name, is_nullable, data_type
+            SELECT column_name, is_nullable, data_type, column_default
             FROM information_schema.columns
             WHERE table_name = %s AND table_schema = %s
             ORDER BY ordinal_position"""
         cursor = self.execute_sql(query, (table, schema or 'public'))
         pks = set(self.get_primary_keys(table, schema))
-        return [ColumnMetadata(name, dt, null == 'YES', name in pks, table)
-                for name, null, dt in cursor.fetchall()]
+        return [ColumnMetadata(name, dt, null == 'YES', name in pks, table, df)
+                for name, null, dt, df in cursor.fetchall()]
 
     def get_primary_keys(self, table, schema=None):
         query = """
@@ -3184,13 +3184,13 @@ class MySQLDatabase(Database):
 
     def get_columns(self, table, schema=None):
         sql = """
-            SELECT column_name, is_nullable, data_type
+            SELECT column_name, is_nullable, data_type, column_default
             FROM information_schema.columns
             WHERE table_name = %s AND table_schema = DATABASE()"""
         cursor = self.execute_sql(sql, (table,))
         pks = set(self.get_primary_keys(table))
-        return [ColumnMetadata(name, dt, null == 'YES', name in pks, table)
-                for name, null, dt in cursor.fetchall()]
+        return [ColumnMetadata(name, dt, null == 'YES', name in pks, table, df)
+                for name, null, dt, df in cursor.fetchall()]
 
     def get_primary_keys(self, table, schema=None):
         cursor = self.execute_sql('SHOW INDEX FROM `%s`' % table)
