coleifer/peewee

commit ff321c514c06858565635279367ecc891852047d

Raise error if attempting to use bulk update w/composite key.

peewee.py
@@ -5144,8 +5144,8 @@ class CompositeKey(MetaField):
         return hash((self.model.__name__, self.field_names))
 
     def __sql__(self, ctx):
-        return ctx.sql(CommaNodeList([self.model._meta.fields[field]
-                                      for field in self.field_names]))
+        return ctx.sql(EnclosedNodeList([self.model._meta.fields[field]
+                                         for field in self.field_names]))
 
     def bind(self, model, name, set_attribute=True):
         self.model = model
@@ -5982,6 +5982,10 @@ class Model(with_metaclass(ModelBase, Node)):
 
     @classmethod
     def bulk_update(cls, model_list, fields, batch_size=None):
+        if isinstance(cls._meta.primary_key, CompositeKey):
+            raise ValueError('bulk_update() is not supported for models with '
+                             'a composite primary key.')
+
         # First normalize list of fields so all are field instances.
         fields = [cls._meta.fields[f] if isinstance(f, basestring) else f
                   for f in fields]

commit 1d284bbbbc454575035d3dde22f63e50ed660ccf

Fix for regression introduced in ff321c5 re: composite pk SQL.

peewee.py
@@ -5157,8 +5157,12 @@ class CompositeKey(MetaField):
         return hash((self.model.__name__, self.field_names))
 
     def __sql__(self, ctx):
-        return ctx.sql(EnclosedNodeList([self.model._meta.fields[field]
-                                         for field in self.field_names]))
+        # If the composite PK is being selected, do not use parens. Elsewhere,
+        # such as in an expression, we want to use parentheses and treat it as
+        # a row value.
+        parens = ctx.scope != SCOPE_SOURCE
+        return ctx.sql(NodeList([self.model._meta.fields[field]
+                                 for field in self.field_names], ', ', parens))
 
     def bind(self, model, name, set_attribute=True):
         self.model = model

tests/regressions.py
@@ -753,10 +753,16 @@ class RD(TestModel):
     value = IntegerField()
     rs = ForeignKeyField(RS, backref='rds')
 
+class RKV(TestModel):
+    key = TextField()
+    value = IntegerField()
+    extra = IntegerField()
+    class Meta:
+        primary_key = CompositeKey('key', 'value')
+
 
 class TestRegressionCountDistinct(ModelTestCase):
-    requires = [RS, RD]
-
+    @requires_models(RS, RD)
     def test_regression_count_distinct(self):
         rs = RS.create(name='rs')
 
@@ -779,3 +785,8 @@ class TestRegressionCountDistinct(ModelTestCase):
 
         query = rs.rds.select(RD.key, RD.value).distinct()
         self.assertEqual(query.count(), 4)  # Was returning 7!
+
+    @requires_models(RKV)
+    def test_regression_count_distinct_cpk(self):
+        RKV.insert_many([('k%s' % i, i, i) for i in range(5)]).execute()
+        self.assertEqual(RKV.select().distinct().count(), 5)