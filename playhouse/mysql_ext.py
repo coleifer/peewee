f76ce0bcadd2d7b7024e2e44614a91c102524d95
Charles Leifer <coleifer@gmail.com>
2019-06-18 15:03:04 -0500

    Add Match helper to playhouse.mysql_ext based on #1955.

--- playhouse/mysql_ext.py
+++ playhouse/mysql_ext.py
@@ -7,7 +7,10 @@ except ImportError:
 
 from peewee import ImproperlyConfigured
 from peewee import MySQLDatabase
+from peewee import NodeList
+from peewee import SQL
 from peewee import TextField
+from peewee import fn
 
 
 class MySQLConnectorDatabase(MySQLDatabase):
@@ -32,3 +35,12 @@ class JSONField(TextField):
     def python_value(self, value):
         if value is not None:
             return json.loads(value)
+
+
+def Match(columns, expr, modifier=None):
+    if isinstance(columns, (list, tuple)):
+        match = fn.MATCH(*columns)  # Tuple of one or more columns / fields.
+    else:
+        match = fn.MATCH(columns)  # Single column / field.
+    args = expr if modifier is None else NodeList((expr, SQL(modifier)))
+    return NodeList((match, fn.AGAINST(args)))
