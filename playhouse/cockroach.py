5d2eab36bf7c3034bcf68240eeec7e88045b212c
Charles Leifer <coleifer@gmail.com>
2019-11-13 16:14:00 -0600

    Implement first-cut of transaction helper / automatic retries.

--- playhouse/cockroach.py
+++ playhouse/cockroach.py
@@ -1,10 +1,20 @@
 import re
 
 from peewee import *
+from peewee import _atomic
 from peewee import ColumnMetadata  # (name, data_type, null, primary_key, table, default)
 from peewee import ForeignKeyMetadata  # (column, dest_table, dest_column, table).
 from peewee import IndexMetadata
 from playhouse.pool import _PooledPostgresqlDatabase
+
+
+TXN_ERR_MSG = ('CockroachDB does not support nested transactions. You may '
+               'alternatively use the @transaction context-manager/decorator, '
+               'which only wraps the outer-most block in transactional logic. '
+               'To run a transaction with automatic retries, use the '
+               'run_transaction() helper.')
+
+class ExceededMaxAttempts(OperationalError): pass
 
 
 class CockroachDatabase(PostgresqlDatabase):
@@ -82,6 +92,55 @@ class CockroachDatabase(PostgresqlDatabase):
         # CRDB does not allow casting a decimal/float to timestamp, so we first
         # cast to int, then to timestamptz.
         return date_field.cast('int').cast('timestamptz')
+
+    def atomic(self):
+        return _crdb_atomic(self)
+
+    def savepoint(self):
+        raise NotImplementedError(TXN_ERR_MSG)
+
+    def retry_transaction(self, cb):
+        def deco():
+            return run_transaction(self, cb)
+        return deco
+
+
+class _crdb_atomic(_atomic):
+    def __enter__(self):
+        if self.db.transaction_depth() > 0:
+            raise NotImplementedError(TXN_ERR_MSG)
+        return super(_crdb_atomic, self).__enter__()
+
+
+def run_transaction(db, callback, max_attempts=None):
+    """
+    Run transactional SQL in a transaction with automatic retries.
+
+    User-provided `callback`:
+    * Must accept one parameter, the `db` instance representing the connection
+      the transaction is running under.
+    * Must not attempt to commit, rollback or otherwise manage transactions.
+    * May be called more than once.
+    * Should ideally only contain SQL write operations.
+
+    Additionally, the database must not have any open transaction at the time
+    this function is called, as CRDB does not support nested transactions.
+    """
+    max_attempts = max_attempts or -1
+    with db.atomic() as txn:
+        db.execute_sql('SAVEPOINT cockroach_restart')
+        while max_attempts != 0:
+            try:
+                result = callback(db)
+                db.execute_sql('RELEASE SAVEPOINT cockroach_restart')
+                return result
+            except OperationalError as exc:
+                if exc.orig.pgcode == '40001':
+                    max_attempts -= 1
+                    db.execute_sql('ROLLBACK TO SAVEPOINT cockroach_restart')
+                    continue
+                raise
+    raise ExceededMaxAttempts(None, 'unable to commit transaction')
 
 
 class PooledCockroachDatabase(_PooledPostgresqlDatabase, CockroachDatabase):
