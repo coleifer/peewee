99921b9b3d8444eec1daf58d4ce06b6210e3aefd
Charles Leifer <coleifer@gmail.com>
2019-12-03 09:36:33 -0600

    Extend postgres_ext jsonb contains to allow field-field comparison.

--- playhouse/postgres_ext.py
+++ playhouse/postgres_ext.py
@@ -318,6 +318,8 @@ class BinaryJSONField(IndexedFieldMixin, JSONField):
     def contains(self, other):
         if isinstance(other, (list, dict)):
             return Expression(self, JSONB_CONTAINS, Json(other))
+        elif isinstance(other, JSONField):
+            return Expression(self, JSONB_CONTAINS, other)
         return Expression(cast_jsonb(self), JSONB_EXISTS, other)
 
     def contained_by(self, other):
