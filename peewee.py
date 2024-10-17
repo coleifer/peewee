f021769722cab323f6e2f431b2a0d3072ef1ff67
Charles Leifer <coleifer@gmail.com>
2024-10-17 07:56:23 -0500

    Always copy the join dict, even if it is empty. Refs #2941

--- peewee.py
+++ peewee.py
@@ -7346,8 +7346,7 @@ class ModelSelect(BaseModelSelect, Select):
 
     def clone(self):
         clone = super(ModelSelect, self).clone()
-        if clone._joins:
-            clone._joins = dict(clone._joins)
+        clone._joins = dict(clone._joins)
         return clone
 
     def select(self, *fields_or_models):
