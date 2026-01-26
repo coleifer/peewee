80dab1a8c72650503780612d0c9fbba86410edb5
Pomin Wu <pomin.wu@proton.me>
2026-01-26 08:26:22 +0800

    Add params for SQL compatibility

--- playhouse/clickhouse.py
+++ playhouse/clickhouse.py
@@ -2,10 +2,14 @@ from peewee import *
 from clickhouse_connect.dbapi.connection import Connection
 from clickhouse_connect.cc_sqlalchemy.datatypes.sqltypes import String
 
+
 class ClickHouseDatabase(Database):
     param = "%s"
 
     def _connect(self):
+        self.connect_params["allow_create_index_without_type"] = 1
+        self.connect_params["create_index_ignore_unique"] = 1
+
         return Connection(database=self.database, **self.connect_params)
 
     def last_insert_id(self, cursor, query_type=None):
