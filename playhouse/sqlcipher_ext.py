da5716e0ecd1ee63e567d395a752ab10c05c1169
Charles Leifer <coleifer@gmail.com>
2026-08-04 21:03:47 -0500

    Clean up old janky docs in sqlcipher extension.

--- playhouse/sqlcipher_ext.py
+++ playhouse/sqlcipher_ext.py
@@ -1,58 +1,28 @@
 """
-Peewee integration with pysqlcipher.
-
-Project page: https://github.com/leapcode/pysqlcipher/
-
-**WARNING!!! EXPERIMENTAL!!!**
-
-* Although this extention's code is short, it has not been properly
-  peer-reviewed yet and may have introduced vulnerabilities.
-
-Also note that this code relies on pysqlcipher and sqlcipher, and
-the code there might have vulnerabilities as well, but since these
-are widely used crypto modules, we can expect "short zero days" there.
+Peewee integration with sqlcipher3.
 
 Example usage:
 
-     from peewee.playground.ciphersql_ext import SqlCipherDatabase
-     db = SqlCipherDatabase('/path/to/my.db', passphrase="don'tuseme4real")
+    from peewee.playground.ciphersql_ext import SqlCipherDatabase
+    db = SqlCipherDatabase('/path/to/my.db', passphrase="don'tuseme4real")
 
-* `passphrase`: should be "long enough".
-  Note that *length beats vocabulary* (much exponential), and even
-  a lowercase-only passphrase like easytorememberyethardforotherstoguess
-  packs more noise than 8 random printable characters and *can* be memorized.
+Invalid or incorrect passphrases do not get a special exception, unfortunately,
+so to catch these in application code you can do something like this:
 
-When opening an existing database, passphrase should be the one used when the
-database was created. If the passphrase is incorrect, an exception will only be
-raised **when you access the database**.
-
-If you need to ask for an interactive passphrase, here's example code you can
-put after the `db = ...` line:
-
-    try:  # Just access the database so that it checks the encryption.
-        db.get_tables()
-    # We're looking for a DatabaseError with a specific error message.
-    except peewee.DatabaseError as e:
-        # Check whether the message *means* "passphrase is wrong"
-        if e.args[0] == 'file is encrypted or is not a database':
-            raise Exception('Developer should Prompt user for passphrase '
-                            'again.')
+    try:
+        db.get_tables()  # Attempt to read from db.
+    except DatabaseError as exc:
+        if 'not a database' in exc.args[0]:
+            print('Invalid passphrase')
         else:
-            # A different DatabaseError. Raise it.
-            raise e
-
-See a more elaborate example with this code at
-https://gist.github.com/thedod/11048875
+            raise
 """
 import datetime
 import decimal
 import sys
 
 from peewee import *
-try:
-    from sqlcipher3 import dbapi2 as sqlcipher
-except ImportError:
-    from pysqlcipher3 import dbapi2 as sqlcipher
+from sqlcipher3 import dbapi2 as sqlcipher
 
 sqlcipher.register_adapter(decimal.Decimal, str)
 sqlcipher.register_adapter(datetime.date, str)
